5f61ba054941b928e94a9a6c029e585151219298
Aaron <>
2025-01-03 10:12:35 +0800

    Combine nsg

--- my-nsg.tf
+++ my-nsg.tf
@@ -5,19 +5,6 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
   resource_group_name = azurerm_resource_group.rg.name
 
   security_rule {
-    name                       = "SSH"
-    priority                   = 300
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = var.source_ip
-    destination_address_prefix = "*"
-    description               = "Allow SSH access from specified IP"
-  }
-
-  security_rule {
     name                       = "DNS"
     priority                   = 310
     direction                  = "Inbound"
@@ -27,25 +14,25 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     destination_port_range     = "53"
     source_address_prefix      = var.source_ip
     destination_address_prefix = "*"
-    description               = "Allow DNS queries from specified IP"
+    description                = "Allow DNS queries from specified IP"
   }
 
   security_rule {
-    name                       = "WEB"
-    priority                   = 320
+    name                       = "AllowFromMyIP"
+    priority                   = 300
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "Tcp"
+    protocol                   = "*"
     source_port_range          = "*"
-    destination_port_ranges    = ["80", "443"]
+    destination_port_ranges    = ["22", "80", "443"]
     source_address_prefix      = var.source_ip
     destination_address_prefix = "*"
-    description               = "Allow HTTP/HTTPS access from specified IP"
+    description                = "Allow access from specified IP"
   }
 
   security_rule {
-    name                       = "PUBLIC"
-    priority                   = 330
+    name                       = "PublicPorts"
+    priority                   = 320
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -53,6 +40,6 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     destination_port_ranges    = ["5000", "5001"]
     source_address_prefix      = "*"
     destination_address_prefix = "*"
-    description               = "Allow application ports from specified IP"
+    description                = "Allow ports from Public"
   }
 }
